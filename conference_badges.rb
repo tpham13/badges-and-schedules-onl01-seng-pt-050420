e4f027b53ded1edfd5e2448d14880cfd4478bfc1
Thanh Pham <tpham0123@gmail.com>
2020-05-12 06:27:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,8 @@ def badge_maker(speakers)
   return "Hello, my name is #{speakers}."
 end
 
-def batch_badge_creator(speakers)
-  speakers.collect do |name|
+def batch_badge_creator(attendees)
+  attendees.collect do |name|
     badge_maker(name)
   end
 
