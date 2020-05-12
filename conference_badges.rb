29f32327cb54a8d4d336e9ba39f3f3d69687bb0d
Thanh Pham <tpham0123@gmail.com>
2020-05-12 06:15:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(speakers)
+  return "Hello, my name is #{speakers}."
+end
+
+def batch_badge_creator(names)
+  names.collect do |name|
+    badge_maker(name)
+  return badge_maker
+end
+end
+
+
+def assign_rooms(speakers)
+  room=0
+  speakers.collect do |name|
+    room+=1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
+
